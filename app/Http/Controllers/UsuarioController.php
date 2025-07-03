9749692903478ecc4e36fec9e7c41fb07f6963f0
unknown <manuelrivero1402@gmail.com>
2025-07-03 09:55:45 +0800

    cree las rutas api para el crud y la base de datos

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function funListar() {
+
+    }
+    public function funGuardar(Request $request) {
+
+    }
+    public function funMostrar($id) {
+
+    }
+    public function funModificar($id,Request $request) {
+
+    }
+    public function funEliminar($id) {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+// crud api rest 
+
+// crud de productos 
+Route::get('/producto',[ProductoController::class, "funListar"]);
+Route::post('/producto',[ProductoController::class, "funGuardar"]);
+Route::get('/producto/{id}',[ProductoController::class, "funMostrar"]);
+Route::put('/producto/{id}',[ProductoController::class, "funModificar"]);
+Route::delete('/producto/{id}',[ProductoController::class, "funEliminar"]);
+
+// crud de usuarios 
+Route::get('/usuario',[UsuarioController::class, "funListar"]);
+Route::post('/usuario',[UsuarioController::class, "funGuardar"]);
+Route::get('/usuario/{id}',[UsuarioController::class, "funMostrar"]);
+Route::put('/usuario/{id}',[UsuarioController::class, "funModificar"]);
+Route::delete('/usuario/{id}',[UsuarioController::class, "funEliminar"]);
+
+// crud categorias 
+// aqui cree el controlador con el prefijo api y me dio las funciones y
+//puedo acceder al apiresource
+Route::apiResource("/categoria", CategoriaController::class);
